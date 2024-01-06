1034c18451a89afb70161e39d5abdc0bdec33691
Roberto <40802138+RobertoPSantos85@users.noreply.github.com>
2024-01-06 16:22:15 -0300

    Criação da IActionResult CreateQrCode

--- Controllers/GeradorController.cs
+++ Controllers/GeradorController.cs
@@ -1,4 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using QRCoder;
+using System.Drawing;
+using System.Drawing.Imaging;
 
 namespace GeradorDeQrCode.Controllers
 {
@@ -11,6 +14,19 @@ namespace GeradorDeQrCode.Controllers
 
         public IActionResult CreateQrCode()
         {
+            var produtoNome = "Arroz";
+            using(MemoryStream stream = new MemoryStream())
+            {
+                QRCodeGenerator qrGenerator = new QRCodeGenerator();
+                QRCodeData qrData = qrGenerator.CreateQrCode(produtoNome, QRCodeGenerator.ECCLevel.Q);
+                QRCode qrCode = new QRCode(qrData);
+
+                using(Bitmap codeBitmap = qrCode.GetGraphic(20))
+                {
+                    codeBitmap.Save(stream, ImageFormat.Png);
+                    ViewBag.QrCodeImage = "data:image/png;base64" + Convert.ToBase64String(stream.ToArray());
+                }
+            }
             return View();
         }
     }
