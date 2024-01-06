8bc654af5868e73a6a1b300c09a03bed6a28dea9
Roberto <40802138+RobertoPSantos85@users.noreply.github.com>
2024-01-06 15:19:37 -0300

    Adicionar arquivos de projeto.

---
+++ Controllers/GeradorController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace GeradorDeQrCode.Controllers
+{
+    public class GeradorController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult CreateQrCode()
+        {
+            return View();
+        }
+    }
+}
---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace GeradorDeQrCode.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
